e5d36e2266ba3c9c7f4993c4055371e373aa0ce1
Shiniko <j_culilap1@u.pacific.edu>
2019-10-10 16:35:53 -0700

    First push

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'hello') {
+    msg.reply('Hiya!')
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
