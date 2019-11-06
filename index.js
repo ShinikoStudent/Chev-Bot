98a0c0eeb943cfa4d66f9f59bbd10447f09425d1
Shiniko <j_culilap1@u.pacific.edu>
2019-11-05 23:30:59 -0800

    Write to sheet functionality works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config()
-const Discord = require('discord.js')
-const client = new Discord.Client()
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
@@ -8,7 +8,9 @@ const {google} = require('googleapis');
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 const TOKEN_PATH = 'token.json';
 
-var val;
+const spreadsheetId = '1MB34CSHSSnm9MFLXTdXqcGuqGTIK4ijEngyUhX7bLIs';
+const PREFIX = 'chev';
+var personScores = [];
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -24,6 +26,19 @@ function authorize(credentials, callback) {
     if (err) return getNewToken(oAuth2Client, callback);
     oAuth2Client.setCredentials(JSON.parse(token));
     callback(oAuth2Client);
+  });
+}
+
+function authorizeWrite(credentials, callback, val1, val2) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, val1, val2);
   });
 }
 
@@ -62,16 +77,54 @@ function getNewToken(oAuth2Client, callback) {
  * @see https://docs.google.com/spreadsheets/d/1MB34CSHSSnm9MFLXTdXqcGuqGTIK4ijEngyUhX7bLIs/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
-function getData(auth){
+function getData(auth) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
-    spreadsheetId: '1MB34CSHSSnm9MFLXTdXqcGuqGTIK4ijEngyUhX7bLIs',
-    range: 'Sheet1!A1:E',
+    spreadsheetId,
+    range: 'Sheet1!A1:B',
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
-    console.log(rows);
-    val = rows;
+    personScores = rows;
+    personScores.shift();
+  });
+}
+
+function addData(auth, score, name) {
+  var resource;
+  var loc = 2;
+  const sheets = google.sheets({version: 'v4', auth});
+
+  sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: 'Sheet1!A2:B',
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const nameList = res.data.values;
+    for (var i = 0; i < nameList.length; i++) {
+      if (nameList[i][0] == name) {
+        loc += i;
+        score += parseInt(nameList[i][1]);
+        let value = score;
+        let values = [[value]]
+        resource = {values};
+      }
+      console.log(nameList[i]);
+    }
+    setTimeout(function(){
+      console.log("this is resource:", resource);
+      var rangeWrite = 'sheet1!B' + loc.toString();
+      console.log(rangeWrite);
+      sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range: rangeWrite,
+        valueInputOption: 'USER_ENTERED',
+        resource,
+      }, (err) => {
+        if (err) return console.log('The API returned an error: ' + err);
+      });
+    }, 1000);
+
   });
 }
 
@@ -85,22 +138,48 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
-  // Load client secrets from a local file.
-  fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Google Sheets API.
-    authorize(JSON.parse(content), getData);
-  });
+  const content = msg.content;
+  const parts = content.split(' ');
 
-  if (msg.content === 'hello') {
-    msg.reply('Hiyaaaaaaaaaa!');
-    /*for (var i = 0; i < val.length; i++){
-      msg.reply(val[i][0]);
-    }*/
+  if (parts[0] != PREFIX){ return;}
+
+  if (msg.content === 'chev list') {
+    // Load client secrets from a local file.
+    fs.readFile('credentials.json', (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err);
+      // Authorize a client with credentials, then call the Google Sheets API.
+      authorize(JSON.parse(content), getData);
+    });
+    msg.reply('No prob, bob! Fetching some data...');
+    var resp = "";
+    for (var i = 0; i < personScores.length; i++) {
+      let name = personScores[i][0];
+      let score = personScores[i][1];
+      let line = name.concat(" -> ", score);
+      resp = resp.concat(line, "\n")
+    }
+
     setTimeout(function(){ 
-      msg.reply(val);
-    }, 1000);
+      var resp = "";
+      for (var i = 0; i < personScores.length; i++) {
+        let name = personScores[i][0];
+        let score = personScores[i][1];
+        let line = name.concat(" -> ", score);
+        resp = resp.concat(line, "\n")
+      }
+      msg.reply("[Name -> score]\n" + resp);
+    }, 2000);
   }
+
+  if (parts[1] === 'add' && parts[2] != null 
+    && parts[3] === 'to' && parts[4] != null) {
+    fs.readFile('credentials.json', (err, content) => {
+      if (err) return console.log('Error loading client secret file:', err);
+      // Authorize a client with credentials, then call the Google Sheets API.
+      authorizeWrite(JSON.parse(content), addData, parseInt(parts[2]), parts[4]);
+    });
+  }
+
 })
 
 client.login(process.env.BOT_TOKEN)
